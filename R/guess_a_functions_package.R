07c78afd5d8ea17c5ed42134a51a779b7414153a
Kyle Harris <koderkow@gmail.com>
2020-06-07 13:44:59 -0500

    small updates

--- R/guess_a_functions_package.R
+++ R/guess_a_functions_package.R
@@ -1,6 +1,6 @@
-#' Where is your home?
+#' @title Where is your home?
 #'
-#' @return invisible
+#' @description  The primary use for this function is for its functionality via addin. The addin can be added as a shortcut; I set it as CMD+; for Mac and CTRL+; for Windows. This will search for possible packages a function belongs to, similar to `?function`. If there are multiple packages that have the same function, this function will favor any of tidyverse packages. This is due to myself generally using the tidyverse functions. If there are multiple tidyverse matchse, it picks the first package based on its alphabetic positioning. If there are no tidyverse packages and other packages, it will still pick the first package that shows up alphabetically.
 guess_a_functions_package <- function() {
 
   context <- rstudioapi::getActiveDocumentContext()
@@ -13,10 +13,12 @@ guess_a_functions_package <- function() {
     return(invisible(selected_text))
   }
 
-  tidyverse_packages <- "1"
+  tidyverse_packages <- "kyle_was_here"
+  tidy_pattern <- "i_like_avacados"
 
+  ## incase, for some reason, tidyverse is not installed
   if ("tidyverse" %in% rownames(installed.packages())) {
-    ## taken from tidyverse::tidyverse_packages(), dont want to include as a dependency
+    ## taken from tidyverse::tidyverse_packages(), I dont want to include as a dependency
     raw <- utils::packageDescription("tidyverse")$Imports
     imports <- strsplit(raw, ",")[[1]]
     parsed <- gsub("^\\s+|\\s+$", "", imports)
@@ -29,7 +31,8 @@ guess_a_functions_package <- function() {
     pattern = paste0("^", selected_text, "$"),
     agrep=FALSE
   ) %>%
-    purrr::pluck("matches", "Package")
+    purrr::pluck("matches", "Package") %>%
+    unique()
 
   tidy_packages <- stringr::str_subset(possible_packages, tidy_pattern, negate = TRUE)
 
